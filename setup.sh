32916cc65d81f1feb05dd6d6ff0a04669e33ad6d
admin <104834960@studnet.swin.edu.au>
2024-09-29 14:33:31 +1000

    Updated setup.sh

--- setup.sh
+++ setup.sh
@@ -8,12 +8,6 @@ sudo apt-get upgrade -y
 sudo apt-get install -y libgl1-mesa-glx libglib2.0-0
 sudo apt install nvidia-cuda-toolkit
 
-
-# Install unzip and unzip file
-sudo apt-get install unzip -y
-## Extract the FlightData.zip file
-unzip FlightData.zip -d ./
-
 # Download and install Anaconda
 ANACONDA_INSTALLER=Anaconda3-2023.03-Linux-x86_64.sh
 wget https://repo.anaconda.com/archive/$ANACONDA_INSTALLER
@@ -29,38 +23,9 @@ conda create -n FlightData python=3.11 -y
 echo "Activating the 'FlightData' environment"
 conda activate FlightData
 
-
-cd ./FlightData
 # Install the packages from requirements.txt
 pip install -r requirements.txt
-pip install tensorflow==2.15.0
-pip install kaggle
-
 
 # Print success message
 echo "Setup complete. Environment 'FlightData' is ready with the required packages."
 echo "Run 'conda activate FlightData' to activate the environment."
-
-
-mkdir -p ~/.kaggle
-mv /FlightData/kaggle.json ~/.kaggle/
-chmod 600 ~/.kaggle/kaggle.json
-
-
-# Check if kaggle.json exists
-if [ ! -f ~/.kaggle/kaggle.json ]; then
-    echo "kaggle.json not found in ~/.kaggle/. Please place your kaggle.json in this directory."
-    exit 1
-fi
-
-# Dataset URL to download
-DATASET_URL="dilwong/flightprices"
-
-# Download the dataset
-kaggle datasets download -d $DATASET_URL
-
-# Unzip the dataset
-unzip itineraries.csv.zip -d flightprices
-
-# Print success message
-echo "Dataset downloaded and unzipped successfully."
